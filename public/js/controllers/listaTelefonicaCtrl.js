26649edc46caae161708304d045453e3c08e4016
Gabriel Cunha <gabriel_mcunha@hotmail.com>
2017-10-02 10:51:56 -0300

    Aula14 - Performance_Part1

--- public/js/controllers/listaTelefonicaCtrl.js
+++ public/js/controllers/listaTelefonicaCtrl.js
@@ -2,6 +2,17 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
 	$scope.app = "Lista Telefonica";
 	$scope.contatos = contatos.data;
 
+	var init = function(){
+		calcularImpostos($scope.contatos);
+		generateSerial($scope.contatos);
+	};
+
+	var calcularImpostos = function(contatos){
+		contatos.forEach(function(contato){
+			contato.operadora.precoComImposto = calcularImposto(contato.operadora.preco);
+		});
+	}
+
 	var generateSerial = function(contatos){
 		contatos.forEach(function(item){
 			item.serial = serialGenerator.generate();
@@ -21,9 +32,11 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
 		$scope.contatos = contatos.filter(function(contato){
 			if (!contato.selecionado) return contato;
 		});
+		$scope.isContatoSelecionado($scope.contatos);
 	};
+	
 	$scope.isContatoSelecionado = function(contatos){
-		return contatos.some(function(contato){
+		$scope.hasContatoSelecionado = contatos.some(function(contato){
 			return contato.selecionado;
 		});
 	};
@@ -31,5 +44,11 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
 		$scope.criterioDeOrdenacao = campo;
 		$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
 	};
-	generateSerial($scope.contatos);
+
+	var calcularImposto = function(preco){
+		var imposto = 1.2;
+		return preco * imposto;
+	};
+
+	init();
 });
